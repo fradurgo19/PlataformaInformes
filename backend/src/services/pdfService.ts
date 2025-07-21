95ba770ecb2898f4d8aa0e3a58116eda412d9e1a
fradurgo19 <fradurgo19@gmail.com>
2025-07-21 10:04:59 -0500

    fix: use chromium.executablePath as getter, not function

--- backend/src/services/pdfService.ts
+++ backend/src/services/pdfService.ts
@@ -208,7 +208,7 @@ export class PDFService {
       const browser = await puppeteer.launch({
         args: chromium.args,
         defaultViewport: chromium.defaultViewport,
-        executablePath: await chromium.executablePath(),
+        executablePath: await chromium.executablePath,
         headless: chromium.headless,
       });
 
